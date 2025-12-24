9d986d949494eff74e8ebf7fa9887b7a6eb54ee9
cristianfloyd <carenas@uba.ar>
2025-12-24 14:24:07 -0300

    refactor: move Item class to a separate file for better organization and maintainability

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,6 @@
 # Constants
+from item import Item
+
 MAX_QUALITY = 50
 MIN_QUALITY = 0
 AGED_BRIE = "Aged Brie"
@@ -22,18 +24,6 @@ MIN_SELL_IN = 0
 NORMAL_DAILY_DECREMENT = 1
 NORMAL_DAILY_INCREMENT = 1
 NORMAL_EXPIRED_DECREMENT = 2
-
-
-class Item:
-    """DO NOT MODIFY - This class belongs to the goblin in the corner."""
-
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)  # noqa: UP031
 
 
 class GildedRose:
---
+++ item.py
@@ -0,0 +1,10 @@
+class Item:
+    """DO NOT MODIFY - This class belongs to the goblin in the corner."""
+
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)  # noqa: UP031
